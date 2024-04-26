beef1c413062adc594ce33ec7bf6161a31d22813
onlinesmit <onlinesmitg@gmail.com>
2024-04-26 17:45:17 +0530

    Performing writeFile operation

--- fileExammp.js
+++ fileExammp.js
@@ -20,17 +20,28 @@ const fs = require('fs');
 // console.log("Terminated: ");   //first print this line because run asyncronyns its execute first that line whose takes less time
 
 
-//2nd method   syncronysn
-const data = fs.readFileSync("./abc.txt", (error, data) => {
+// //2nd method   syncronysn
+// const data = fs.readFileSync("./abc.txt", (error, data) => {
 
-  if(error){
-    console.log("error", error);
-   }else{
-    // console.log(data);  //output oin the form of buffer
-    console.log(data.toString());  //change into string    output in the form of String
+//   if(error){
+//     console.log("error", error);
+//    }else{
+//     // console.log(data);  //output oin the form of buffer
+//     console.log(data.toString());  //change into string    output in the form of String
+//   }
+// });
+
+
+// console.log("Terminated: ");   //after executing above line then jump to tghis line and 2nd time print this line because run syncronyns its execute first that line whose asign work first in syncronync
+
+//writeFile
+
+let content = "Wao this the dynamic content: "
+fs.writeFile("new_file.txt", content, (err) => {
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log("Saved: ");
   }
 });
-
-
-console.log("Terminated: ");   //after executing above line then jump to tghis line and 2nd time print this line because run syncronyns its execute first that line whose asign work first in syncronync
-
