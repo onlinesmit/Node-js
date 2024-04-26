aa29ad61f3a1b94f75624e0b09989bcfb6f3f270
onlinesmit <onlinesmitg@gmail.com>
2024-04-26 17:31:09 +0530

    performing the readfile operation

---
+++ fileExammp.js
@@ -0,0 +1,36 @@
+//performing read and write operation
+
+const fs = require('fs');
+
+//readfile  writefile   appendfile   delete file
+
+// // 1st mathod   ansyncronync
+// fs.readFile("./abc.txt", (error, data) => {
+
+//   if(error){
+//     console.log("error");
+//     console.log(error);
+//   }else{
+//     // console.log(data);  //output oin the form of buffer
+//     console.log(data.toString());  //change into string    output in the form of String
+//   }
+// });
+
+
+// console.log("Terminated: ");   //first print this line because run asyncronyns its execute first that line whose takes less time
+
+
+//2nd method   syncronysn
+const data = fs.readFileSync("./abc.txt", (error, data) => {
+
+  if(error){
+    console.log("error", error);
+   }else{
+    // console.log(data);  //output oin the form of buffer
+    console.log(data.toString());  //change into string    output in the form of String
+  }
+});
+
+
+console.log("Terminated: ");   //after executing above line then jump to tghis line and 2nd time print this line because run syncronyns its execute first that line whose asign work first in syncronync
+
