0a76e9f2d24ecc5aed04f9b4cbef235e4d79954a
onlinesmit <onlinesmitg@gmail.com>
2024-04-28 11:39:45 +0530

    Peroforming basic programming by creating mdules

---
+++ useModule/programming.js
@@ -0,0 +1,9 @@
+// const d = require("./programmingmod.js")
+
+// console.log( d.sub(3, 5));
+
+// we can also write like this
+const {add, sub} = require("./programmingmod.js")
+
+console.log(sub(3, 5));
+console.log(add(3, 5));
