091d2c67ec3474672ec1b4be5363568f3af6b6ca
Smit Kumar <onlinesmitg@gmail.com>
2024-05-25 23:53:48 +0530

    Working on webserver

--- web server/index.js
+++ web server/index.js
@@ -1,9 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 
 const myServer = http.createServer((req, res) => {
   // console.log("New request receive");
-  console.log(req);
-  res.end("Hello from server");
+  // console.log(req);
+  const log = `${Date.now()}: New req receive`;
+  fs.appendFile('log.txt', log, (err, data) => {
+    res.end("Hello from server");
+  }); 
 });
 
 myServer.listen(8001, () => {
