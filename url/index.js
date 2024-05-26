9595d549c9936acd20348bd431f241f07b1c8815
Smit Kumar <onlinesmitg@gmail.com>
2024-05-26 23:40:20 +0530

    refactor: Update web server to handle different routes

---
+++ url/index.js
@@ -0,0 +1,11 @@
+ //https://www.google.com/search?q=javascript+url+module+example
+
+ //https: is a protocol
+ //www.google.com is a host
+    //search is a path
+    //q is a query
+    //javascript+url+module+example is a query string
+
+//ws is a web socket use to create a real time communication between server and client
+
+//https is a protocol s stands for secure it is used to create a secure connection between server and client it has ssh certificate
--- web server/index.js
+++ web server/index.js
@@ -6,7 +6,16 @@ const myServer = http.createServer((req, res) => {
   // console.log(req);
   const log = `${Date.now()}: ${req.url}New req received\n`;
   fs.appendFile('log.txt', log, (err, data) => {
-    res.end("Hello from server again");
+    switch (req.url) {
+      case '/':
+        res.end("Home Page");
+        break;
+      case '/about':
+        res.end("Hello from about");
+        break;
+      default:
+        res.end("404 page not found");
+    }
   }); 
 });
 
